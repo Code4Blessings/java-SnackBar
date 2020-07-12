e6ed12dfa71e2ac9b9ffc180307f8d8570f66c7c
Code4Blessings <robinwarden@code4blessings.com>
2020-07-12 10:40:03 -0400

    Created Man.java file and compiled

---
+++ SnackBar/src/snackBar/Main.java
@@ -0,0 +1,14 @@
+package snackBar;
+
+public class Main 
+{
+    private static void workWithData()
+    {
+        System.out.println("Go on girl!!! Learn that Java!!!");
+    }
+
+    public static void main(String[] args) 
+    {
+        workWithData();   
+    }
+}
